a0aa02cec8bcd04f7cc50b600d38b91eb8ae5657
bbogeul <bbogeul@gmail.com>
2015-05-21 14:05:06 -0700

    testing for mvptms

---
+++ common/routing.js
@@ -0,0 +1,7 @@
+Router.map(function(){
+    this.route('home', {path: '/'} );
+    this.route('about', {path: '/about-us'});
+    this.route('team', {path: '/meet-the-crew'});
+     this.route('emr', {path: '/emr'});
+    this.route('practice', {path: '/what-we-do'});
+});
